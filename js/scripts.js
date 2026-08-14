ad45f62d3b2fddf8871315d668cc757da4f081f5
Rodel Dahay <odedahay@gmail.com>
2026-08-14 00:55:46 -0600

    added js and improve responsiveness

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,17 @@
 const navToggle = document.querySelector(".nav__toggle");
 const navMenu = document.querySelector(".nav__menu");
+const nav = document.querySelector(".nav");
+const announcement = document.querySelector(".announcement");
+
+if (nav) {
+    const updateStickyNav = () => {
+        const stickyOffset = announcement ? announcement.offsetHeight : 0;
+        nav.classList.toggle("is-sticky", window.scrollY > stickyOffset);
+    };
+
+    updateStickyNav();
+    window.addEventListener("scroll", updateStickyNav, { passive: true });
+}
 
 if (navToggle && navMenu) {
     navToggle.addEventListener("click", () => {
